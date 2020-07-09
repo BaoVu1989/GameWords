a71c19bbfb9c8b8404b285e997adf730ed636649
Danny-Vu <vudinhbao.nz@gmail.com>
2020-07-09 12:00:51 +0700

    Initial Commit

---
+++ GameWordsSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GameWordsSwiftUI
+//
+//  Created by Bao Vu on 7/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
